d514a5593bbaa9fd009d018a6a4a80fbb4fc36c9
Joseph Boscan <joeypc.py@gmail.com>
2021-12-22 20:07:07 -0400

    Fixing the player

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ function App() {
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
   const RESPONSE_TYPE = "token";
 
+  // https://accounts.spotify.com/authorize?client_id=8b945ef10ea24755b83ac50cede405a0&response_type=code&redirect_uri=http://localhost:3000&
+
   const [token, setToken] = useState("");
 
   const logout = () => {
@@ -38,6 +40,8 @@ function App() {
     console.log(token);
   };
 
+  const getHistorial = () => {};
+
   return (
     <div
       className="App login-view"
@@ -45,9 +49,9 @@ function App() {
     >
       <div className="app-header">
         {!token && (
-           <a
+          <a
             className="btn"
-            href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope`}
+            href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`}
           >
             Login to Spotify
           </a>
@@ -59,6 +63,9 @@ function App() {
             <Dashboard token={token} />
             <button className="btn-logout" onClick={logout}>
               Logout
+            </button>
+            <button className="btn-logout" onClick={getHistorial}>
+              Get historial
             </button>
           </div>
         )}
